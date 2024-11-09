ab5b3f83db10563530454193a7a5ca317739a7c9
YunJaeHoon <sleepzzz214@naver.com>
2024-11-09 17:23:52 +0900

    FIX: Profile 엔티티 수정

--- src/main/java/com/example/KU_2024_hackathon/exception/CustomErrorCode.java
+++ src/main/java/com/example/KU_2024_hackathon/exception/CustomErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum CustomErrorCode
 {
-    ALREADY_USED_ID(HttpStatus.BAD_REQUEST, "이미 사용 중인 이메일입니다.");
+    ALREADY_USED_EMAIL(HttpStatus.BAD_REQUEST, "이미 사용 중인 이메일입니다."),
+    EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "이메일을 찾을 수 없습니다."),
+    INVALID_COLOR_CODE(HttpStatus.BAD_REQUEST, "색상 코드는 ");
 
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/com/example/KU_2024_hackathon/service/ProfileService.java
+++ src/main/java/com/example/KU_2024_hackathon/service/ProfileService.java
@@ -6,17 +6,20 @@ import com.example.KU_2024_hackathon.exception.CustomErrorCode;
 import com.example.KU_2024_hackathon.exception.CustomException;
 import com.example.KU_2024_hackathon.repository.ProfileRepository;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
 import java.time.LocalDateTime;
 
 @Service
+@RequiredArgsConstructor
 public class ProfileService
 {
-    @Autowired ProfileRepository profileRepository;
-    @Autowired BCryptPasswordEncoder encoder;
+    private final ProfileRepository profileRepository;
+    private final BCryptPasswordEncoder encoder;
 
     /* 회원가입 서비스 */
     @Transactional
@@ -25,7 +28,7 @@ public class ProfileService
         // 해당 아이디로 생성된 계정이 이미 존재한다면 예외 처리
         profileRepository.findByEmail(dto.getEmail())
                 .ifPresent(user -> {
-                    throw new CustomException(CustomErrorCode.ALREADY_USED_ID, dto.getEmail());
+                    throw new CustomException(CustomErrorCode.ALREADY_USED_EMAIL, dto.getEmail());
                 });
 
         // Profile 엔티티 생성
@@ -40,4 +43,31 @@ public class ProfileService
         // 데이터베이스에 계정 추가
         profileRepository.save(profile);
     }
+
+    /* 감정 색상 수정 서비스
+    @Transactional
+    public void updateColor(Principal principal, ProfileDto.UpdateColor dto)
+    {
+        // 로그인 여부 확인 요청을 보낸 사용자의 계정 이메일
+        String email = principal.getName();
+
+        // 해당 사용자의 계정이 존재하는지 확인
+        Profile profile = profileRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException(CustomErrorCode.EMAIL_NOT_FOUND, email));
+
+        if(
+            dto.getJoy().length() != 6 ||
+            dto.getAngry().length() != 6 ||
+            dto.getSad().length() != 6 ||
+            dto.getAfraid().length() != 6 ||
+            dto.getAdmiration().length() != 6 ||
+            dto.getSurprise().length() != 6 ||
+            dto.getInterest().length() != 6 ||
+            dto.getBoring().length() != 6
+        ) {
+
+        }
+        profile.setJoy(dto.getJoy());
+    }
+    */
 }
